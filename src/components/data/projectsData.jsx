782309e19fc9e2978da47b2a49276ded5badc3b4
didierMaba <deus_maba@yahoo.fr>
2023-11-28 18:57:47 +0100

    correct import pix

--- src/components/data/projectsData.jsx
+++ src/components/data/projectsData.jsx
@@ -1,4 +1,6 @@
-import Pict from "../../../public/assets/img/assos.jpg";
+import assosPix from "../../../public/assets/img/assos.jpg";
+import carPix from "../../../public/assets/img/supakar.png";
+import portPix from "../../../public/assets/img/portfolioDWWM.png";
 
 export const projectsData = [
   {
@@ -8,7 +10,7 @@ export const projectsData = [
     languages: ["Html", "CSS", "Php", "Javascript", "MySQL"],
     infos:
       "Une Association à but non lucratif régie par la loi du 1er juillet 1901 et le décret du 16 août 1901, ayant pour titre Association Woté Pamodja.",
-    img: {Pict},
+    img: assosPix,
     link: "http://maba.alwaysdata.net/index.php",
   },
   {
@@ -18,7 +20,7 @@ export const projectsData = [
     languages: ["Html", "Css", "React"],
     infos:
       "Une application créée dans le cadre d'un exercice et test au deploiement pour la mise en production.",
-    img: "../../../public/assets/img/supakar.png",
+    img: carPix,
     link: "https://superkart-1wuzs2ccu-didiers-projects.vercel.app/",
   },
   {
@@ -27,7 +29,7 @@ export const projectsData = [
     date: "Avril 2023",
     languages: ["Html", "Css", "Javascript"],
     infos: "Il s'agit de mon portfolio DWWM crée en Html, Css et Javascript .",
-    img: "../../../public/assets/img/portfolioDWWM.png",
+    img: portPix,
     link: "https://didiermaba.github.io/PortFolioJs/",
   },
 ];
